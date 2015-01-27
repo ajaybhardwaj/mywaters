8815d4aae1c7976d2c0bfda3aab2f34bffabf6f3
ajaybhardwaj <ajaybhardwaj@live.in>
2015-01-27 10:30:55 +0800

    Initial Commit

---
+++ MyWaters/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWaters
+//
+//  Created by Ajay Bhardwaj on 27/1/15.
+//  Copyright (c) 2015 iAppsAsia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyWaters/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWaters
+//
+//  Created by Ajay Bhardwaj on 27/1/15.
+//  Copyright (c) 2015 iAppsAsia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
